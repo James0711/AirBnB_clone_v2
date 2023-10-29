d008ea6a82bb9511054b95c2396f7e79fcf8e1e5
James0711 <jengreat94@gmail.com>
2023-10-29 04:25:38 -0700

    updates

---
+++ regen_mysql_dev_test.sql
@@ -0,0 +1,7 @@
+-- Deletes the entire db
+
+DROP DATABASE IF EXISTS hbnb_dev_db;
+DROP DATABASE IF EXISTS hbnb_test_db;
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
